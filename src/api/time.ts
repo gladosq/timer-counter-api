accc960b10efd6dc14fccff2d2d959b1145be065
gladosq <gladosq@gmail.com>
2023-04-11 05:12:14 +0400

    changed api key timezone

--- src/api/time.ts
+++ src/api/time.ts
@@ -1,6 +1,6 @@
 export const timeFetcher = async () => {
     const res = await fetch(
-        'https://timezone.abstractapi.com/v1/current_time/?api_key=0153d30904324028b7e200b4fdff21c3&location=Oxford, United Kingdom',
+        'https://timezone.abstractapi.com/v1/current_time/?api_key=7b1951fd0e5341a8afce8c5ec4bb33f1&location=Oxford, United Kingdom',
         {
             method: 'GET',
             headers: {'Content-type': 'application/json; charset=UTF-8'},
