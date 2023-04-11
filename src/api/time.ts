a3f42991f356c5d07005ca969a3388f20a6ba276
gladosq <gladosq@gmail.com>
2023-04-11 06:28:11 +0400

    change protocol

--- src/api/time.ts
+++ src/api/time.ts
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 export const timeFetcher = async () => {
     return axios.get(
-        'http://api.timezonedb.com/v2.1/get-time-zone?key=I2VJQ0NZWK14&format=json&by=zone&zone=America/Chicago',
+        'https://api.timezonedb.com/v2.1/get-time-zone?key=I2VJQ0NZWK14&format=json&by=zone&zone=America/Chicago',
         {
             method: 'GET',
-            // body: null,
             headers: {'Content-type': 'application/json',},
 
 
